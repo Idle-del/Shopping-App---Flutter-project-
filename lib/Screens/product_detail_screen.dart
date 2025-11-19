159045cae93094515297d1719416788782b003c5
Idle del <magarkiran436@gmail.com>
2025-11-19 11:54:25 +0545

    added scroll view

--- lib/Screens/product_detail_screen.dart
+++ lib/Screens/product_detail_screen.dart
@@ -15,65 +15,67 @@ class ProductDetailScreen extends StatelessWidget {
         title: Text(product.title),
         backgroundColor: Colors.orangeAccent,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(
-              product.image,
-              height: 200,
-              fit: BoxFit.cover,
-              errorBuilder: (context, error, stackTrace) {
-                return const Icon(Icons.error, size: 200);
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                product.image,
+                height: 200,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return const Icon(Icons.error, size: 200);
+                },
+              ),
+              const SizedBox(height: 16),
+              Text(product.title,
+                  style:
+                      const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 8),
+              Text('\$${product.price}',
+                  style: const TextStyle(fontSize: 18, color: Colors.green)),
+              const SizedBox(height: 8),
+              Text('Category: ${product.category}',
+                  style: const TextStyle(fontSize: 16)),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  const Text('Rating: ', style: TextStyle(fontSize: 16)),
+                  Text('${product.rating}',
+                      style: const TextStyle(fontSize: 16, color: Colors.amber)),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Text(product.description),
+        
+              const SizedBox(height: 30,),
+        
+              ElevatedButton(onPressed: (){
+                 Cart.addProduct(product);
+                 showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            backgroundColor: Colors.white,
+                            title: const Text("SUCESS", style: TextStyle(
+                              color: Colors.green
+                            ),),
+                            content: const Text("product added to Cart!"),
+                            actions: [
+                              TextButton(
+                                  onPressed: () => Navigator.of(context).pop(),
+                                  child: const Text("OK", style: TextStyle(
+                                    color: Colors.lightBlue
+                                  ),))
+                            ],
+                          );
+                        });
               },
-            ),
-            const SizedBox(height: 16),
-            Text(product.title,
-                style:
-                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 8),
-            Text('\$${product.price}',
-                style: const TextStyle(fontSize: 18, color: Colors.green)),
-            const SizedBox(height: 8),
-            Text('Category: ${product.category}',
-                style: const TextStyle(fontSize: 16)),
-            const SizedBox(height: 8),
-            Row(
-              children: [
-                const Text('Rating: ', style: TextStyle(fontSize: 16)),
-                Text('${product.rating}',
-                    style: const TextStyle(fontSize: 16, color: Colors.amber)),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Text(product.description),
-
-            const SizedBox(height: 30,),
-
-            ElevatedButton(onPressed: (){
-               Cart.addProduct(product);
-               showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          backgroundColor: Colors.white,
-                          title: const Text("SUCESS", style: TextStyle(
-                            color: Colors.green
-                          ),),
-                          content: const Text("product added to Cart!"),
-                          actions: [
-                            TextButton(
-                                onPressed: () => Navigator.of(context).pop(),
-                                child: const Text("OK", style: TextStyle(
-                                  color: Colors.lightBlue
-                                ),))
-                          ],
-                        );
-                      });
-            },
-             child: const Text("Add To Cart"))
-          ],
+               child: const Text("Add To Cart"))
+            ],
+          ),
         ),
       ),
     );
